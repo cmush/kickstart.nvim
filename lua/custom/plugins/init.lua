5a2576dcdbca7866a0d4c611131d4ae76b99c61c
cmush <collinsmucheru@gmail.com>
2024-07-07 14:30:43 +0300

    add neotree + toggle

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,7 +10,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         component_separators = '|',
         section_separators = '',
       },
@@ -19,5 +19,18 @@ return {
         lualine_z = { 'tabs' },
       },
     },
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>b', ':Neotree filesystem toggle<CR>')
+    end,
   },
 }
