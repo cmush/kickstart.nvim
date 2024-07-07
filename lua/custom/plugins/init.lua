df9564122e41c4b73e19ef9a60cec9806e3431d1
cmush <collinsmucheru@gmail.com>
2024-07-07 12:12:18 +0300

    set lualine as statusline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    -- set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- see `:help lualine.txt`
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        icons_enabled = false,
+        component_separators = '|',
+        section_separators = '',
+      },
+      tabline = {
+        lualine_a = { 'buffers' },
+        lualine_z = { 'tabs' },
+      },
+    },
+  },
+}
